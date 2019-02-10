d445ce63060c02e1e03e6261df88c049f0fe3163
Dotcom <sneakjunkmail@gmail.com>
2019-02-10 15:35:44 -0500

    added to modules

--- src/app/menu-items.ts
+++ src/app/menu-items.ts
@@ -2,7 +2,7 @@ import { Menu } from './menu';
 
 export const MENU: Menu[] = [
 	{ id: 1, name:'Modules'},
-	{ id: 2, name:'Templating'},
-	{ id: 3, name:'Components'},
+	{ id: 2, name:'Components'},
+	{ id: 3, name:'Templating'},
 	{ id: 4, name:'Data-Binding'},
 ];
--- src/app/modules/modules.component.ts
+++ src/app/modules/modules.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modules.component.css']
 })
 export class ModulesComponent implements OnInit {
-	const title = 'Modules';
+	title = 'Modules';
 
   constructor() { }
 
